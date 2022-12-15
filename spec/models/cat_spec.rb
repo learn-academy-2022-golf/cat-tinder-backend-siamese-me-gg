0a264a47ff230ff7924934cc620fc96e1ffb1f7e
gmgreer <beauski8@gmail.com>
2022-12-15 14:06:46 -0800

    mistake to put on main

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate presence of name" do
+    cat = Cat.create 
+    expect(cat.errors[:name]).to_not be_empty
+  end
+  it "should validate presence of age" do
+    cat = Cat.create 
+    expect(cat.errors[:age]).to_not be_empty
+  end
+  it "should validate presence of enjoys" do
+    cat = Cat.create 
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+  it "should validate presence of image" do
+    cat = Cat.create 
+    expect(cat.errors[:image]).to_not be_empty
+  end
+  it "should validate minimum length of enjoys" do
+    cat = Cat.create name: "spot", age: 12, enjoys: "not h", image:"random" 
+    expect(cat.errors[:enjoys]).to include "is too short (minimum is 10 characters)"
+    expect(cat.errors[:enjoys]).to_not be_empty
+    
+
+  end
 end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -14,9 +14,9 @@ RSpec.describe "Cats", type: :request do
   let(:invalid_attributes) do
     {
       'name' => '',
-      'age' => 'boy',
+      'age' => "",
       'enjoys' => '',
-      'image' => 3
+      'image' => ''
     }
   end
 
@@ -40,6 +40,22 @@ RSpec.describe "Cats", type: :request do
         end.to change(Cat, :count).by(1)
       end
     end
+    context 'with invalid parameters' do
+      it 'does not create a new Cat and shows an invalid response' do
+        expect do
+          post cats_url, params: { cat: invalid_attributes }
+          expect(response).to have_http_status(422)
+          # Convert the JSON response into a Ruby Hash
+          json = JSON.parse(response.body)
+          # Errors are returned as an array because there could be more than one, if there are more than one validation failures on an attribute.
+          expect(json['name']).to include "can't be blank"
+          expect(json['enjoys']).to include "can't be blank"
+          expect(json['age']).to include "can't be blank"
+          expect(json['image']).to include "can't be blank"
+          expect(json['enjoys']).to include "is too short (minimum is 10 characters)"
+        end.to change(Cat, :count).by(0)
+      end
+    end
   end
 
   describe 'PATCH /update' do
@@ -59,9 +75,21 @@ RSpec.describe "Cats", type: :request do
         cat.save
         patch cat_url(cat), params: { cat: new_attributes }
         cat.reload
-        expect(response).to be_successful
         expect(response).to have_http_status(200)
         expect(cat.name).to eq 'New'
+      end
+    end
+    context 'with invalid parameters' do
+      it "shows an invalid error message and can not update" do
+        cat = Cat.create valid_attributes
+        patch cat_url(cat), params: { cat: invalid_attributes }
+        json = JSON.parse(response.body)
+        expect(json['name']).to include "can't be blank"
+        expect(json['enjoys']).to include "can't be blank"
+        expect(json['age']).to include "can't be blank"
+        expect(json['image']).to include "can't be blank"
+        expect(json['enjoys']).to include "is too short (minimum is 10 characters)"
+        expect(response).to have_http_status(422)
       end
     end
   end
